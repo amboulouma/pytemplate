a212af028ccc00b06b33118a45c7acf3b9691ab3
Amine M. Boulouma <amine@boulouma.com>
2018-07-09 09:56:35 +0200

    Added token regex

--- constants.py
+++ constants.py
@@ -4,9 +4,3 @@ VAR_TOKEN_START = '{{'
 VAR_TOKEN_END = '}}'
 BLOCK_TOKEN_START = '{%'
 BLOCK_TOKEN_END = '%}'
-TOK_REGEX = re.compile(r"(%s.*?%s|%s.*?%s)" % (
-    VAR_TOKEN_START,
-    VAR_TOKEN_END,
-    BLOCK_TOKEN_START,
-    BLOCK_TOKEN_END
-))
